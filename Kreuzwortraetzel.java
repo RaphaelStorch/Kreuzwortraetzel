d2711d142562fd4127198182d506ea4e2c147073
Raphael Storch <Raphael-Lukas.Storch@informatik.hs-fulda.de>
2020-10-23 08:54:02 +0200

    first commit - commiting Kreuzwortraetzel

---
+++ Kreuzwortraetzel.java
@@ -0,0 +1,10 @@
+import java.util.Scanner; 
+public class Kreuzwoertraetzel{
+public static void main(String[] args){
+	Scanner scan=new Scanner(System.in);
+	String name; 
+	int tag; 
+
+	System.out.println("Hallo Hallo!");
+	Scanner.nextLine();
+}}
