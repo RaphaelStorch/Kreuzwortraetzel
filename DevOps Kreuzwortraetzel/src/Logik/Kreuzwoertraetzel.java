3e310f5bc5f519e202fab288fc9826ef0020a21a
Raphael Storch <Raphael-Lukas.Storch@informatik.hs-fulda.de>
2020-11-03 14:53:32 +0100

    added eclipse workspace

---
+++ DevOps Kreuzwortraetzel/src/Logik/Kreuzwoertraetzel.java
@@ -0,0 +1,11 @@
+package Logik;
+import java.util.Scanner; 
+public class Kreuzwoertraetzel{
+public static void main(String[] args){
+	Scanner scan=new Scanner(System.in);
+	String name; 
+	int tag; 
+
+	System.out.println("Hallo Hallo!");
+	System.out.println("Test Teset");
+}}
