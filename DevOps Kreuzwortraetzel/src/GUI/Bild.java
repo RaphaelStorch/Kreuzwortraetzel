0f7fde78b6c5a1a0d1099b3367da9967034c22c9
Raphael Storch <Raphael-Lukas.Storch@informatik.hs-fulda.de>
2020-12-08 18:07:07 +0100

    new stuff by testing around

--- DevOps Kreuzwortraetzel/src/GUI/Bild.java
+++ DevOps Kreuzwortraetzel/src/GUI/Bild.java
@@ -24,7 +24,8 @@ public class Bild extends JFrame {
 					frame.setVisible(true);
 				} catch (Exception e) {
 					e.printStackTrace();
-			////Hall Wie gehts sasd 
+			////Halsadfasdfl Wie gehts sasd asdfasdf
+					dasdasdsdasd
 				}
 			}
 		});
