25609d4af388a4b1bd3078f990a367bacb00140c
Raphael Storch <73331317+RaphaelStorch@users.noreply.github.com>
2021-01-25 19:42:59 +0100

    cleaned Space

--- DevOps Kreuzwortraetzel/src/Logik/Main.java
+++ DevOps Kreuzwortraetzel/src/Logik/Main.java
@@ -3,7 +3,7 @@ package Logik;
 import JFrames.Tutorial;
 
 public class Main {
-	private static Tutorial updater = new Tutorial();
+	public static Tutorial updater = new Tutorial();
 	
 	public static void main(String[] args) {
 		init();
